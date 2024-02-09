0472a494ec97e850fe56525e4501b3d8843ff496
VGardacho2022 <106764881+VGardacho2022@users.noreply.github.com>
2024-02-09 16:32:45 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-02-09T00:00:00");
+const tempoObjetivo1 = new Date("2024-02-20T00:00:00");
+let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1;
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
 for(let i=0;i <botoes.length;i++){
     botoes[i].onclick = function(){
@@ -16,5 +17,15 @@ for(let i=0;i <botoes.length;i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+    return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
 }
 
